9dee0adc25744a2ad5db77fc6cbfa49ac179544a
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-06-01 18:34:44 +0300

    add express for folder-public, add middleware for avatar, in user-schema add avatarURL, npm packets for avatars, function for updating avatar for user, add gitkeep

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const { validateBody, authenticate } = require("../../decorators");
 
+const { upload } = require("../../middleWares");
+
 const userController = require("../../controllers/auth-controllers");
 
 const { schemas } = require("../../models/user");
@@ -15,5 +17,7 @@ router.post("/login", validateBody(schemas.loginJoiSchema), userController.login
 router.get("/current", authenticate, userController.getCurrent);
 
 router.post("/logout", authenticate, userController.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), userController.updateAvatar);
 
 module.exports = router;
